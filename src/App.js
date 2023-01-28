cc6ddf5c1c0d6d96233083dcc6de404954bf01e6
Yuqian-Cao <laineyyangli@gmail.com>
2023-01-28 14:48:11 -0600

    me

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>RYAN {value}</div>;
+  return <div>Hello this is an edit YUQIAN {value}</div>;
+>>>>>>> 38737ba (me)
 }
 
 export default App;
