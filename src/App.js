542b4914a37ad5c8978f2173356c380633a871f4
Casey <91210043+peicasey@users.noreply.github.com>
2023-01-28 17:32:43 -0600

    updated, used variables and imports

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     
     <div>
       <h1 className="intro">
-        Hello world!
+        Hello {value}!
       </h1>
 
       
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
-//   const handleClick = () => setNav(!nav);
 
   return (
     <div>
