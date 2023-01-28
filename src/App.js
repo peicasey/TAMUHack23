6325327e3f53538ee686959a9232dd8c8fc9d05f
Casey <91210043+peicasey@users.noreply.github.com>
2023-01-28 14:16:51 -0600

    changed app.js to see if it worked

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello this is an edit {value}</div>;
 }
 
 export default App;
