5c9d47dbfa4963b711c6315f235f549cbcfa9de9
LilyTTT <yushantang4@gmail.com>
2023-01-29 09:59:14 -0600

    Add items page

--- src/components/AddItems.js
+++ src/components/AddItems.js
@@ -6,7 +6,65 @@ const AddItem = () => {
 
   return (
     <div className="bg">
-        Add Item
+      <h1>Bag</h1>
+      <Form>
+        <FormGroup>
+          <Label for="length">
+            Length
+          </Label>
+          <Input
+            id="length"
+            name="length"
+            placeholder="Enter the length of this bag"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="width">
+            Width
+          </Label>
+          <Input
+            id="width"
+            name="width"
+            placeholder="Enter the width of this bag"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="height">
+            Height
+          </Label>
+          <Input
+            id="height"
+            name="height"
+            placeholder="Enter the height of this bag"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup check>
+          <Input
+            name="carryOn"
+            type="radio"
+          />
+          {' '}
+          <Label check>
+            Carry On
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+          <Input
+            name="checked"
+            type="radio"
+          />
+          {' '}
+          <Label check>
+            Checked
+          </Label>
+        </FormGroup>
+      </Form>
+      <Button>
+        Add Another Bag
+      </Button>
     </div>
   );
 };
