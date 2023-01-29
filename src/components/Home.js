291602e03f1625415f5005f00f6d0cd98e16cc83
Casey <91210043+peicasey@users.noreply.github.com>
2023-01-29 10:54:07 -0600

    hopefully fixed home page buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,26 @@
 import React from "react";
 
 // import CollapseTxt from "./CollapseTxt";
-import { Button, Collapse, Alert } from 'reactstrap';
+import { Button,
+    Accordion,
+    AccordionBody,
+    AccordionHeader,
+    AccordionItem,
+  } from 'reactstrap';
 import will from '../assets/will_925x782.png'; // gives image path
 
 import '../App.css';
 
 const Home = () => {
+
+    const [open, setOpen] = useState('1');
+    const toggle = (id) => {
+        if (open === id) {
+        setOpen();
+        } else {
+        setOpen(id);
+        }
+    };
 
   return (
     <div className="flex flex-col items-center mt-16 bg">
@@ -20,72 +34,46 @@ const Home = () => {
         <div>
             <img src={will} alt="Will the Packmate" width="700" height="550" className="center"/>
             <div className="flex flex-col items-center">
-            <div>
-            <Button
-                color="info"
-                onClick={function noRefCheck(){}}
-                style={{
-                marginBottom: '1rem'
-                }}
-            >
-                WHY
-            </Button>
-            <Collapse horizontal>
-                <Alert
-                style={{
-                    width: '500px'
-                }}
-                >
-                Find out about what TSA Guidelines might apply to you!
-                </Alert>
-            </Collapse>
+            <Accordion open={open} toggle={toggle}>
+                <AccordionItem>
+                <AccordionHeader targetId="1">Accordion Item 1</AccordionHeader>
+                <AccordionBody accordionId="1">
+                    <strong>This is the first item&#39;s accordion body.</strong>
+                    You can modify any of this with custom CSS or overriding our default
+                    variables. It&#39;s also worth noting that just about any HTML can
+                    go within the <code>.accordion-body</code>, though the transition
+                    does limit overflow.
+                </AccordionBody>
+                </AccordionItem>
+                <AccordionItem>
+                <AccordionHeader targetId="2">Accordion Item 2</AccordionHeader>
+                <AccordionBody accordionId="2">
+                    <strong>This is the second item&#39;s accordion body.</strong>
+                    You can modify any of this with custom CSS or overriding our default
+                    variables. It&#39;s also worth noting that just about any HTML can
+                    go within the <code>.accordion-body</code>, though the transition
+                    does limit overflow.
+                </AccordionBody>
+                </AccordionItem>
+                <AccordionItem>
+                <AccordionHeader targetId="3">Accordion Item 3</AccordionHeader>
+                <AccordionBody accordionId="3">
+                    <strong>This is the third item&#39;s accordion body.</strong>
+                    You can modify any of this with custom CSS or overriding our default
+                    variables. It&#39;s also worth noting that just about any HTML can
+                    go within the <code>.accordion-body</code>, though the transition
+                    does limit overflow.
+                </AccordionBody>
+                </AccordionItem>
+            </Accordion>
             </div>
-            <div>
-            <Button
-                color="info"
-                onClick={function noRefCheck(){}}
-                style={{
-                marginBottom: '1rem'
-                }}
-            >
-                CHOOSE
-            </Button>
-            <Collapse horizontal>
-                <Alert
-                style={{
-                    width: '500px'
-                }}
-                >
-                Learn sneaky Airline Guidelienes!
-                </Alert>
-            </Collapse>
-            </div>
-            <div>
-            <Button
-                color="info"
-                onClick={function noRefCheck(){}}
-                style={{
-                marginBottom: '1rem'
-                }}
-            >
-                US?
-            </Button>
-            <Collapse horizontal>
-                <Alert
-                style={{
-                    width: '500px'
-                }}
-                >
-                Have a worry-free travel experience with luggage that has already been checked
-                </Alert>
-            </Collapse>
-            </div>
-            </div>
+            <div className="center">
             <Button
                 color="info"
                 size="lg"
-                START A NEW TRIP NOW
-            ></Button>
+                
+            >START A NEW TRIP NOW</Button>
+            </div>
         </div>
     </div>
   );
