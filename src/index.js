478a82387c46b062247dd675182eb1e760d63176
Casey <91210043+peicasey@users.noreply.github.com>
2023-01-29 07:07:55 -0600

    add root stuff

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
+
+const root = createRoot(document.getElementById("root"));
 
 ReactDOM.render(
     <BrowserRouter>
