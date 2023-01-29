f667f1ae9e7c4183732df7fd4a85c64fda8e1912
Casey <91210043+peicasey@users.noreply.github.com>
2023-01-28 18:01:47 -0600

    attempting to test out tailwind again

--- src/App.js
+++ src/App.js
@@ -10,15 +10,16 @@ import React from 'react';
 function App() {
   const value = 'World';
   return (
-    
     <div>
-      <h1 className="intro">
-        Hello {value}!
-      </h1>
-
-      
       <Navbar />
-      <Home />
+      <div className="flex flex-col items-center mt-16">
+        <h1 className="text-4xl text-center">
+          Tailwind CSS makes styling React components easier!
+        </h1>
+      <button className="bg-black text-white p-2.5 w-fit mt-9 ">
+        Get Started
+      </button>
+      </div>
     </div>
   );
 
